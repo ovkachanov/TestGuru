26df19c3694bd7f11e8817c80c638e189d665ea5
Oleg Kachanov <ovkachanov1988@gmail.com>
2020-02-14 18:16:06 +0300

    add default values to Answer#correct & Tets#level

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_150159) do
+ActiveRecord::Schema.define(version: 2020_02_14_151154) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "correct", default: true
   end
 
   create_table "categories", force: :cascade do |t|
@@ -34,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_02_14_150159) do
     t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "level", null: false
+    t.integer "level", default: 1, null: false
   end
 
   create_table "users", force: :cascade do |t|
