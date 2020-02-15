a45b74c0a2353c8668d1da119e372ce8e34ab2aa
Oleg Kachanov <ovkachanov1988@gmail.com>
2020-02-15 13:52:19 +0300

    add reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_151154) do
+ActiveRecord::Schema.define(version: 2020_02_15_104816) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -23,10 +23,8 @@ ActiveRecord::Schema.define(version: 2020_02_14_151154) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "test_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["test_id"], name: "index_categories_on_test_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -42,6 +40,8 @@ ActiveRecord::Schema.define(version: 2020_02_14_151154) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "level", default: 1, null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
