e1f5e551c7b6af46436a11bf29b6aef0390e28f3
Oleg Kachanov <ovkachanov1988@gmail.com>
2020-02-15 20:26:24 +0300

    lesson_4

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+
+  def self.sorting_tests_by_category(name)
+    self.joins(:category).where(categories: { title: name}).order('tests.title DESC')
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_104816) do
+ActiveRecord::Schema.define(version: 2020_02_15_161944) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -41,7 +41,9 @@ ActiveRecord::Schema.define(version: 2020_02_15_104816) do
     t.datetime "updated_at", null: false
     t.integer "level", default: 1, null: false
     t.integer "category_id"
+    t.integer "user_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
