06da1af23c49811ca14ac995e367c17087207ce9
Oleg Kachanov <ovkachanov1988@gmail.com>
2020-02-15 08:53:23 +0300

    add reference Test-Category, Test-Question, Question-Answer

--- db/migrate/20200214144528_create_categories.rb
+++ db/migrate/20200214144528_create_categories.rb
@@ -2,6 +2,7 @@ class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
       t.string :title
+      t.references :test, foreign_key: true, index: true
 
       t.timestamps
     end
