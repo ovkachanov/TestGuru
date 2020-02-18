379c231e522ae6fba2a3fa18e28796c56dcac333
Oleg Kachanov <ovkachanov1988@gmail.com>
2020-02-18 10:37:55 +0300

    fix lesson6

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,12 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
+  scope :correct_awnser, -> { where(correct: true)}
+
   validates :body, presence: true
+  validate :validate_quantity_answer, on: :create
+
+  def validate_quantity_answer
+    errors.add(:answers_count, message: "Invalid number of answers") if self.question.answers.count > 4
+  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -7,8 +7,12 @@ class Test < ApplicationRecord
   scope :easy_test, -> { where(level: 0..1) }
   scope :middle_test, -> { where(level: 2..4) }
   scope :hard_test, -> { where(level: 5..Float::INFINITY) }
-  scope :sorting_tests_by_category, -> (name) { joins(:category).where(categories: { title: name}).order('tests.title DESC') }
+  scope :sorting_tests_by_category, -> (name) { joins(:category).where(categories: { title: name}) }
 
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
   validates :title, presence: true, uniqueness: { scope: :level }
+
+  def self.by_title(title)
+    sorting_tests_by_category(title).order(title: :desc).pluck(:title)
+  end
 end
