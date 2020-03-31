a62fcd7468b730a66c4480f8cc1ee384e0ce38c9
Oleg Kachanov <ovkachanov1988@gmail.com>
2020-03-31 18:03:57 +0300

    delete migration

--- db/migrate/20200215104816_add_and_delete_reference.rb
+++ db/migrate/20200215104816_add_and_delete_reference.rb
@@ -1,6 +1,4 @@
 class AddAndDeleteReference < ActiveRecord::Migration[5.2]
   def change
-    add_foreign_key :tests, :categories, index: true
-    remove_foreign_key :categories, column: :test_id
   end
 end
