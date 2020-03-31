5d27bafacc6c04bb5738e5bc383b843fe89df54d
Oleg Kachanov <ovkachanov1988@gmail.com>
2020-03-31 18:22:09 +0300

    edit migration

--- db/migrate/20200331104603_add_reference_key.rb
+++ db/migrate/20200331104603_add_reference_key.rb
@@ -1,6 +1,4 @@
 class AddReferenceKey < ActiveRecord::Migration[5.2]
   def change
-    remove_foreign_key :tests, :categories
-    add_foreign_key :tests, :categories, column: :category_id
   end
 end
